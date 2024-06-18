5f48d5e053fffc657550f13a77910b3ec0d23d89
Priyal Sanap <priyalsanap24041995@gmail.com>
2024-06-18 17:56:35 -0500

    initial commit

---
+++ Project 1/.ipynb_checkpoints/project1-checkpoint.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band name generator.")
+City= input("Whcih city you grew up in?\n")
+Pet= input("What is the name of the pet?\n")
+print("Your band name could be "+ City + " " + Pet)
